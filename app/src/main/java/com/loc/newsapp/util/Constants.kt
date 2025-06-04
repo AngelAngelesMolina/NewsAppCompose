61c34b064843ec0d9aac64ef75e6c5a325ef5ddb
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-06-03 21:09:03 -0600

    NewsPagingSource

--- app/src/main/java/com/loc/newsapp/util/Constants.kt
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -3,4 +3,5 @@ package com.loc.newsapp.util
 object Constants {
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
+    const val API_KEY = "b2139932c19c4656b80493696042a2c9"
 }
