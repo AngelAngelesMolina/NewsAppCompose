4b24716b927287cee38d28050d7b4bd0f8e4bd7d
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-06-06 13:14:42 -0600

    HomeViewModel

--- app/src/main/java/com/loc/newsapp/util/Constants.kt
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
     const val API_KEY = "b2139932c19c4656b80493696042a2c9"
+    const val BASE_URL = "https://newsapi.org/v2/"
 }
