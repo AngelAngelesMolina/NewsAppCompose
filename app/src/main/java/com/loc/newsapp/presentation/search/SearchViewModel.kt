3523519e1164dbde5480d8ec024d5089b6aad363
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-06-10 17:13:21 -0600

    Create SearchViewModel.kt

---
+++ app/src/main/java/com/loc/newsapp/presentation/search/SearchViewModel.kt
@@ -0,0 +1,43 @@
+package com.loc.newsapp.presentation.search
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.loc.newsapp.domain.usecases.news.NewsUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import androidx.paging.cachedIn
+
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(private val newsUseCases: NewsUseCases) : ViewModel() {
+
+
+    private val _state = mutableStateOf(SearchState())
+    private val state: State<SearchState> = _state
+
+    fun onEvent(event: SearchEvents) {
+        when (event) {
+            is SearchEvents.UpdateSearchQuery -> {
+                _state.value = state.value.copy(searchQuery = event.searchQuery)
+            }
+
+            is SearchEvents.SearchNews -> {
+                searchNews()
+            }
+
+            SearchEvents.NavigateUp -> TODO()
+            is SearchEvents.OnError -> TODO()
+        }
+
+    }
+
+    private fun searchNews() {
+        val articles = newsUseCases.searchNews(
+            searchQuery = state.value.searchQuery,
+            sources = listOf("bbc-news", "abc-news", "al-jazeera-english")
+        ).cachedIn(viewModelScope)
+
+    }
+}
