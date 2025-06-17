6f7aa16bb035f499a84b4bdcd9b800da9b57f6af
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-06-16 20:18:06 -0600

    dao usesCases

--- app/src/main/java/com/loc/newsapp/domain/usecases/news/DeleteArticle.kt
+++ app/src/main/java/com/loc/newsapp/domain/usecases/news/DeleteArticle.kt
@@ -2,11 +2,12 @@ package com.loc.newsapp.domain.usecases.news
 
 import com.loc.newsapp.data.local.NewsDao
 import com.loc.newsapp.domain.model.Article
+import javax.inject.Inject
 
-class UpsertArticle( private val newsDao: NewsDao) {
+class DeleteArticle @Inject constructor(private val newsDao: NewsDao) {
 
     suspend operator fun invoke(article: Article) {
-        newsDao.upsert(article)
+        newsDao.delete(article)
     }
 
 }
